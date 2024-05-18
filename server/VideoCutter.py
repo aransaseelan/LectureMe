5a19e2429836beb2cf3d0de00edc1f316ab26135
janahan <janahansiva6@gmail.com>
2024-05-18 18:47:22 -0400

    chore: Refactor VideoCutter to use moviepy library for video cutting

--- server/VideoCutter.py
+++ server/VideoCutter.py
@@ -1,3 +1,11 @@
+from moviepy.editor import *
 
-def videoCutter():
+def videoCutter(keyPoints, video):
     '''Cuts up the video using the key points'''
+    clips = [];
+    for i in keyPoints:
+        # Cut the video at the given time
+        clip = VideoFileClip(video).subclip(i[0], (i[0]+i[1]))
+        clips.append(clip)
+        
+    return clips
