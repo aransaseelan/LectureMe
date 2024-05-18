75024ad48476cf1ee440a7060c36a852e7f68c2e
Kaven Raj <kaven.rajakumar@gmail.com>
2024-05-18 13:53:58 -0400

    Updated requirements.txt and TranscriptAPI

--- server/Run.py
+++ server/Run.py
@@ -4,15 +4,16 @@ from MainPointsAPI import MainPoints
 from VideoCutter import videoCutter
 '''Runner of the Python program.'''
 
+def get_clipped_videos(youtube_link):
+    # '''Goes to VideoFile which gets the youtube link from the user'''
+    youtubeLink = get_link()
 
-youtubeLink = get_link()
+    '''Goes to TranscriptAPI which extracts the transcript from the video with time stamps'''
+    transcript = videoTranscript(youtubeLink)
 
-'''Goes to TranscriptAPI which extracts the transcript from the video with time stamps'''
-transcript = videoTranscript(youtubeLink)
+    '''Goes to OpenAI API which will determine the key points of the video transcript'''
+    mainPoints = MainPoints(transcript)
 
-'''Goes to OpenAI API which will determine the key points of the video transcript'''
-mainPoints = MainPoints(transcript)
-
-'''Cuts up the video using the key points'''
-#VideoCutter = VideoCutter(youtubeLink, mainPoints)
+    '''Cuts up the video using the key points'''
+    #VideoCutter = VideoCutter(youtubeLink, mainPoints)
     
--- server/TranscriptAPI.py
+++ server/TranscriptAPI.py
@@ -1,5 +1,29 @@
-''''''
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
 
-def videoTranscript(video_id):
+# def main():
+#     videoTranscript('https://www.youtube.com/watch?v=HCOQmKTFzYY&t=51s')
+
+def videoTranscript(youtube_link):
     # Get the transcript of the video
-    transcript = []
+    video_id = extract_video_id(youtube_link)
+    # transcript = YouTubeTranscriptApi.get_transcript(video_id)
+    # print(transcript)
+    print(video_id)
+
+def extract_video_id(url):
+    # Regular expression pattern to match YouTube video IDs
+    pattern = re.compile(r'^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*')
+    
+    # Match the pattern with the given URL
+    match = pattern.match(url)
+    
+    # Check if a match is found and the length of the video ID is 11
+    if match and len(match.group(2)) == 11:
+        return match.group(2)
+    else:
+        # Handle error (e.g., raise an exception, return None, etc.)
+        return None
+
+# if __name__ == '__main__':
+#     main()
