6f6d2f11f68aa30c4f3fb7edf9acb3e8c5460ded
janahan <janahansiva6@gmail.com>
2024-05-18 12:15:02 -0400

    created frontend folder

---
+++ frontend/lectureme/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div id="header">
+          <h1 id="main-text">LectureMe</h1>
+          <h1 id="log-in">log in</h1>
+          <div id="get-started">
+            get started
+          </div>
+        </div>
+        <div id="hero">
+          <h1 id="hero-text">Learn <span id="gradient-text">smarter</span>. Use your time <span id="gradient-text">better</span>.<br />Get <span id="gradient-text">LectureMe</span>.</h1>
+          <div id="glass-cover">
+            <img id="tv" src="television.png" alt="tv"></img>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
